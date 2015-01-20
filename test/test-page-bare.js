69f5d4b6bf264e9e29f9bc211c2c9655e1e4cb3d
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2015-01-19 23:55:16 -0800

    More refactoring of the page bare test

--- test/test-page-bare.js
+++ test/test-page-bare.js
@@ -5,28 +5,32 @@ describe('react-material-ui:page-bare', function () {
   describe('no options', function () {
     var react;
     var appPath = 'myapp';
+    var deps = [
+      '../../../page-bare'
+    ];
+    var options = {
+      'appPath': appPath,
+      'skip-welcome-message': true,
+      'skip-install': true,
+      'skip-message': true
+    };
 
     beforeEach(function (done) {
-      var deps = [
-        '../../../page-bare'
-      ];
       helpers
         .testDirectory(path.join(__dirname, '.tmp', appPath), function (err) {
           if (err) { return done(err); }
-          var generator =
-            (path.basename(path.dirname(__dirname)) + ':page-bare')
-              .slice('generator-'.length);
-          react = helpers.createGenerator(generator, deps, ['game player'], {
-            'appPath': appPath,
-            'skip-welcome-message': true,
-            'skip-install': true,
-            'skip-message': true
-          });
           done();
         });
     });
 
     it('should generate an entirely new application', function () {
+      var generator =
+        (path.basename(path.dirname(__dirname)) + ':page-bare')
+          .slice('generator-'.length);
+
+      react = helpers
+        .createGenerator(generator, deps, ['game player'], options);
+
       react.run({}, function () {
         helpers.assertFile([
           'src/pages/GamePlayerPage/index.js',
