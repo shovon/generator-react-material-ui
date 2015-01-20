20813fd9adef6158635b723c02d27b8590ab3a7a
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2015-01-19 22:06:54 -0800

    Refactored the view test.

--- test/test-view.js
+++ test/test-view.js
@@ -5,45 +5,48 @@ describe('react-material-ui:view', function () {
   describe('no options', function () {
     var react;
     var appPath = 'myapp';
+    var deps = [
+      '../../../view'
+    ];
+
+    var options = {
+      'appPath': appPath,
+      'skip-welcome-message': true,
+      'skip-install': true,
+      'skip-message': true
+    };
 
     beforeEach(function (done) {
-      var deps = [
-        '../../../view'
-      ];
       helpers
         .testDirectory(path.join(__dirname, '.tmp', appPath), function (err) {
           if (err) { return done(err); }
-          var generator =
-            (path.basename(path.dirname(__dirname)) + ':view')
-              .slice('generator-'.length);
-          react = helpers.createGenerator(generator, deps, ['game player'], {
-            'appPath': appPath,
-            'skip-welcome-message': true,
-            'skip-install': true,
-            'skip-message': true
-          });
           done();
         });
     });
 
     it('should generate an entirely new application', function () {
-      react.run({}, function () {
-        helpers.assertFile([
-          'src/views/GamePlayerView/style.less',
-          'src/views/GamePlayerView/index.js',
-          'src/views/GamePlayerView/__tests__/index-test.js',
-        ]);
+      var generator =
+        (path.basename(path.dirname(__dirname)) + ':view')
+          .slice('generator-'.length);
 
-        helpers.assertFileContent(
-          'src/views/GamePlayerView/index.js',
-          /\s+<div className='game-player-view'>\n\s+Game Player/
-        );
+      helpers.createGenerator(generator, deps, ['game player'], options)
+        .run({}, function () {
+          helpers.assertFile([
+            'src/views/GamePlayerView/style.less',
+            'src/views/GamePlayerView/index.js',
+            'src/views/GamePlayerView/__tests__/index-test.js',
+          ]);
 
-        helpers.assertFileContent(
-          'src/views/GamePlayerView/style.less',
-          /\.game-player-view/
-        );
-      });
+          helpers.assertFileContent(
+            'src/views/GamePlayerView/index.js',
+            /\s+<div className='game-player-view'>\n\s+Game Player/
+          );
+
+          helpers.assertFileContent(
+            'src/views/GamePlayerView/style.less',
+            /\.game-player-view/
+          );
+        });
     });
   });
 });
